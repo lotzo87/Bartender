7e3876cb2b7e5d071bb4c33a72d78d16d1f5939d
victor <victor_riojas@yahoo.com.mx>
2016-10-03 22:53:50 -0500

    Initial Commit

---
+++ Bartender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bartender
+//
+//  Created by Victor Hugo Vázquez Riojas on 10/3/16.
+//  Copyright © 2016 Victor Hugo Vázquez Riojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BartenderTests/BartenderTests.swift
@@ -0,0 +1,36 @@
+//
+//  BartenderTests.swift
+//  BartenderTests
+//
+//  Created by Victor Hugo Vázquez Riojas on 10/3/16.
+//  Copyright © 2016 Victor Hugo Vázquez Riojas. All rights reserved.
+//
+
+import XCTest
+@testable import Bartender
+
+class BartenderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
